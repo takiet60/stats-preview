651be5d42796ce332b6eb60b7dba51d92db8e458
TaAnhKiet <18130119@st.hcmuaf.edu.vn>
2022-09-23 20:09:35 +0700

    Complete code for stats preview

--- stats-preview/src/App.js
+++ stats-preview/src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/main.scss'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main className="wrapper">
+      <article className="card">
+        <header className="card__header">
+          <span className="card__header-overlay"></span>
+        </header>
+        <div className="card__body">
+          <h1 className="card__title">
+            Get <span className="violet">insights</span> that help your business grow.
+          </h1>
+          <p className="card__des">
+            Discover the benefits of data analytics and make better decisions regarding revenue, customer
+            experience, and overall efficiency.
+          </p>
+          <div className="card__stats-container">
+            <p className="card__stats">
+              <span className="card__stat">10k+</span> companies
+            </p>
+            <p className="card__stats">
+              <span className="card__stat">314</span> templates
+            </p>
+            <p className="card__stats">
+              <span className="card__stat">12m+</span> queries
+            </p>
+          </div>
+        </div>
+      </article>
+
+    </main>
+
   );
 }
 
